0105ab03a04560056444c99cbb88dbaf129ac987
AdamJastrz <106631230+AdamJastrz@users.noreply.github.com>
2022-12-20 00:49:58 +0100

    Refractoring

--- index.js
+++ index.js
@@ -1,89 +1,52 @@
 "use strict";
 
-let id = 1,
-  suit = "clubs",
-  rank = 2;
+let rank = 2;
 let deck = [];
+let suits = ["clubs", "diamonds", "hearts", "spades"];
 
 let player1Deck = [],
-  player2Deck = [];
+    player2Deck = [];
 
-function Card(id, suit, rank) {
-  this.id = id;
-  this.suit = suit;
-  this.rank = rank;
-}
-
-function pushCard() {
-  const newCard = new Card(id, suit, rank);
-  deck.push(newCard);
+function pushCard(suit, rank) {
+    const newCard = {
+        suit,
+        rank,
+    };
+    deck.push(newCard);
 }
 
 function createDeck() {
-  for (let i = 1; i <= 52; i++) {
-    if (i >= 1 && i <= 13) {
-      suit = "clubs";
-      id = i;
-      pushCard();
-      rank++;
-    } else if (i >= 14 && i <= 26) {
-      switch (i) {
-        case 14:
-          rank = 2;
-      }
-      suit = "diamonds";
-      id = i;
-      pushCard();
-      rank++;
-    } else if (i >= 27 && i <= 39) {
-      switch (i) {
-        case 27:
-          rank = 2;
-      }
-      suit = "hearts";
-      id = i;
-      pushCard();
-      rank++;
-    } else if (i >= 40 && i <= 52) {
-      switch (i) {
-        case 40:
-          rank = 2;
-      }
-      suit = "spades";
-      id = i;
-      pushCard();
-      rank++;
+    for (let suit of suits) {
+        rank = 2;
+        for (let i = 1; i <= 13; i++) {
+            pushCard(suit, rank);
+            rank++;
+        }
     }
-  }
-  console.log(deck);
 }
 
-function shuffleArr() {
-  for (let i = deck.length - 1; i > 0; i--) {
-    let rand = Math.floor(Math.random() * (i + 1));
-    [deck[i], deck[rand]] = [deck[rand], deck[i]];
-  }
-  console.log(deck);
+function shuffleArr(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() * (i + 1));
+        [array[i], array[rand]] = [array[rand], array[i]];
+    }
+    console.log(array);
 }
 
-function pickTrump() {
-  const trump = deck[deck.length - 1].suit;
-  console.log(trump);
-  const storage = deck.pop();
-  deck.unshift(storage);
-  console.log(deck);
+function pickTrump(deck) {
+    return deck[0].suit;
 }
 
 function setup() {
-  for (let i = 1; i <= 6; i++) {
-    player1Deck.push(deck.pop());
-    player2Deck.push(deck.pop());
-  }
-  console.log(player1Deck);
-  console.log(player2Deck);
+    for (let i = 1; i <= 6; i++) {
+        player1Deck.push(deck.pop());
+        player2Deck.push(deck.pop());
+    }
+    console.log(player1Deck);
+    console.log(player2Deck);
 }
 
 createDeck();
-shuffleArr();
-pickTrump();
+shuffleArr(deck);
+pickTrump(deck);
 setup();
